2de56f4fe94f164b17cbe1821cc3ab4855bfe7f3
Mahidhar Nuthanapati <46488704+prometheus6011@users.noreply.github.com>
2023-09-07 15:56:21 -0400

    Arrays introduction

---
+++ Intro_to_DSA/array.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class array<T> {
+
+
+
+    public static void main(String[] args) {
+        int[] myArray = new int[5];
+        myArray[0] = 5;
+        myArray[1] = 1;
+        myArray[2] = 8;
+        myArray[3] = 10;
+        myArray[4] = 2;
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}
